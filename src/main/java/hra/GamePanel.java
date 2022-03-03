adf202081c8111f28243f0ae210f8bd1ecf18f4f
Dominik <dominikbielik239@gmail.com>
2022-03-04 00:00:02 +0100

    basic veci do 8 min

---
+++ src/main/java/hra/GamePanel.java
@@ -0,0 +1,46 @@
+package hra;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+    GamePanel(){
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
---
+++ src/main/java/hra/SnakeGame.java
@@ -0,0 +1,8 @@
+package hra;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();    //GameFrame frame= new GameFrame();
+    }
+}
